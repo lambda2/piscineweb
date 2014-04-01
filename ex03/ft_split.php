b2a9cff9118e4bca40998bbfff32796dad8cb652
Andre Aubin <andre.aubin@lambdaweb.fr>
2014-04-01 14:27:03 +0200

    Adding ex03

---
+++ ex03/ft_split.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+function ft_split($str)
+{
+	function clean_element($a)
+	{
+		return (trim($a));
+	}
+
+	function drop_element($a)
+	{
+		return (strlen($a));
+	}
+
+	$cleaned = array_map("clean_element", explode(" ", $str));
+	$filtered = array_filter($cleaned, "drop_element");
+	sort($filtered);
+	return ($filtered);
+}
+?>
