63022844dd767add1a3dc350095d59cab3f1f55d
Andre Aubin <andre.aubin@lambdaweb.fr>
2014-04-03 18:27:01 +0200

    fixing 02

--- ex02/loupe.php
+++ ex02/loupe.php
@@ -1,26 +1,68 @@
 #!/usr/bin/php
 <?php
+
+/*
+
+Tentatives...
+
+<a+((?<attr>\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>([^<]*)(<\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)>)?<\/a> => pas trop mal...
+
+
+*/
 function link_up($file)
 {
+
+	function replaceTitle($str)
+	{
+		/**
+		 * Regex for match title attributes
+		 */
+		$regex = "~<a(?P<beforetag>\s+(.*=.*)title=[\"\'])"
+		."(?P<title>[^\"\'<>]+[\"\'])|"
+		."(?P<aftertag>\w+=[\"\'][^\"\'<>]+[\"\'])"
+		."+>([^<]*)<\/a>~Uim";
+		$e = preg_replace_callback(
+			$regex,
+			function($match)
+			{
+				$s = "<a".$match["beforetag"];
+				if ($match["title"])
+					$s .= strtoupper($match["title"]);
+				$s .= $match["aftertag"];
+				return $s;
+			},
+			$str
+		);
+		return ($e);
+	}
+
+	function replaceText($str)
+	{
+		$regex = "~(?<beforetext><a\s[^>]*>)(?<text>[^<]*)"
+				."(?<aftertext>(<([A-Za-z][A-Za-z0-9]*)"
+				."\s[^>]*>(.*?)<\/\1>)?(.*)?<\/a>)~im";
+		$e = preg_replace_callback(
+			$regex,
+			function($match)
+			{
+				$s = $match["beforetext"];
+				if ($match["text"])
+					$s .= strtoupper($match["text"]);
+				$s .= $match["aftertext"];
+				return $s;
+			},
+			$str
+		);
+		return ($e);
+	}
+
 	if (file_exists($file))
 	{
 		$str = file_get_contents($file);
 		if ($str)
 		{
-			$regex = "~(?P<open><a)(?P<hreftag> href=(?P<href>['|\"].*['|\"]))?"
-					.">(?P<label>.*)(?P<close><\/a>)~Ui";
-			$e = preg_replace_callback(
-				$regex,
-				function($match)
-				{
-					$s = $match["open"];
-					if ($match["hreftag"])
-						$s .= " href=".strtoupper($match["href"]);
-					$s .= ">".strtoupper($match["label"]).$match["close"];
-					return $s;
-				},
-				$str);
-			echo "$e\n";
+			$str = replaceText(replaceTitle($str));
+			echo $str."\n";
 		}
 	}
 }
