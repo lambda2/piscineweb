d7edc0cfb83c3c422b37ffa275da90c1b19cb2c7
Andre Aubin <andre.aubin@lambdaweb.fr>
2014-04-05 03:39:04 +0200

    Adding base for ex03

---
+++ ex03/modif.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Will return an array containing all elements of the given
+ * serialized database.
+ */
+function getDb($file = "../private/passwd")
+{
+	if (file_exists($file))
+		$db = unserialize(file_get_contents($file));
+	else
+	{
+		if (!file_exists("../private"))
+			mkdir("../private");
+		$db = array();
+	}
+	return ($db);
+}
+
+/**
+ * Will save the given db array into the given file,
+ * in a serialized format.
+ */
+function saveDb($db, $file = "../private/passwd")
+{
+	if (!file_exists("../private"))
+		mkdir("../private");
+	$content = serialize($db);
+	return (file_put_contents($file, $content));
+}
+
+/**
+ * Will check that the forms arguments are here and corrects
+ */
+function checkFormArgs($f)
+{
+	return (
+		$f and isset($f["submit"]) and $f["submit"] = "ok"
+		and isset($f["login"]) and $f["login"] != ""
+		and isset($f["passwd"]) and $f["passwd"] != ""
+	);
+}
+
+/**
+ * Will check if the given login exists in the given $db array
+ */
+function loginExists($login, $db)
+{
+	$logins = array_reduce($db, function($e, $item) {
+		if ($item) {
+			$e[] = $item["login"];
+		}
+		return ($e);
+	}, array());
+	return (in_array($_POST["login"], $logins));
+}
+
+/*
+ * Si on a qque chose dans le formulaire
+ */
+if (checkFormArgs($_POST))
+{
+	$db = getDb();
+
+	if (loginExists($_POST["login"], $db))
+	{
+		echo "ERROR\n";
+	}
+	else
+	{
+		// Create a new array with the user login and an hashed password.
+		$infos = array(
+			"login" => $_POST["login"],
+			"passwd" => hash("sha256", $_POST["passwd"])
+		);
+		$db[] = $infos;
+		saveDb($db);
+		echo "OK\n";
+	}
+}
+else
+{
+	echo "ERROR\n";
+}
+?>
